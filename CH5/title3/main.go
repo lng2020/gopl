40d371ad8c87bb3f85df54337207701b2db5ce19
lng2020 <nanguanlin6@gmail.com>
2022-10-31 20:12:08 +0800

    code for CH5

---
+++ CH5/title3/main.go
@@ -0,0 +1,46 @@
+package title3
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+)
+
+func soleTitle(doc *html.Node) (title string, err error) {
+	type bailout struct{}
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+			// no panic
+		case bailout{}:
+			err = fmt.Errorf("multiple title elements")
+		default:
+			panic(p)
+		}
+	}()
+
+	forEachNode(doc, func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" &&
+			n.FirstChild != nil {
+			if title != "" {
+				panic(bailout{})
+			}
+			title = n.FirstChild.Data
+		}
+	}, nil)
+	if title == "" {
+		return "", fmt.Errorf("no title element")
+	}
+	return title, nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
