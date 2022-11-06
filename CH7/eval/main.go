43bf9474acd4efb5f3a2c7595858b8c9dccf7bef
lng2020 <nanguanlin6@gmail.com>
2022-11-06 22:29:06 +0800

    code for CH7

---
+++ CH7/eval/main.go
@@ -0,0 +1,26 @@
+package eval
+
+type Var string
+
+type literal float64
+
+type unary struct {
+	op rune
+	x  Expr
+}
+
+type binary struct {
+	op   rune
+	x, y Expr
+}
+
+type call struct {
+	fn   string
+	args []Expr
+}
+
+type Env map[Var]float64
+
+type Expr interface {
+	Eval(env Env) float64
+}
